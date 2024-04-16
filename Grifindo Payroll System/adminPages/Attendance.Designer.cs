365315ae19bc0851a1ef8f0ba4b9b7eb04f301f8
Shan Romesh <shanromesh7@outlook.com>
2024-04-16 21:25:57 +0530

    adding new employee and attendance components

---
+++ Grifindo Payroll System/adminPages/Attendance.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Grifindo_Payroll_System.Pages
+{
+    partial class Attendance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CommonPal = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.advancedPIC = new System.Windows.Forms.PictureBox();
+            this.empPIC = new System.Windows.Forms.PictureBox();
+            this.salaryPIC = new System.Windows.Forms.PictureBox();
+            this.bonousPIC = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.homePIC = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.advancedPIC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.empPIC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salaryPIC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bonousPIC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.homePIC)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CommonPal
+            // 
+            this.CommonPal.FormStyles.FormMain.StateCommon.Back.Color1 = System.Drawing.Color.White;
+            this.CommonPal.FormStyles.FormMain.StateCommon.Back.Color2 = System.Drawing.Color.White;
+            this.CommonPal.FormStyles.FormMain.StateCommon.Border.Color1 = System.Drawing.Color.White;
+            this.CommonPal.FormStyles.FormMain.StateCommon.Border.Color2 = System.Drawing.Color.White;
+            this.CommonPal.FormStyles.FormMain.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.CommonPal.FormStyles.FormMain.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.AntiAlias;
+            this.CommonPal.FormStyles.FormMain.StateCommon.Border.Rounding = 15;
+            this.CommonPal.FormStyles.FormMain.StateCommon.Border.Width = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.advancedPIC);
+            this.panel1.Controls.Add(this.empPIC);
+            this.panel1.Controls.Add(this.salaryPIC);
+            this.panel1.Controls.Add(this.bonousPIC);
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.homePIC);
+            this.panel1.Location = new System.Drawing.Point(-1, -5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(76, 851);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.panel2.Location = new System.Drawing.Point(17, 373);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(44, 5);
+            this.panel2.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Poppins SemiBold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(17, 517);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 19);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Salary";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Poppins SemiBold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(15, 439);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 19);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Bonous";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Poppins SemiBold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(5, 597);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 19);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Advanced";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Poppins SemiBold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(15, 357);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 19);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Attend";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poppins SemiBold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 19);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Employee";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poppins SemiBold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 19);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Home";
+            // 
+            // advancedPIC
+            // 
+            this.advancedPIC.Image = global::Grifindo_Payroll_System.Properties.Resources.settings;
+            this.advancedPIC.Location = new System.Drawing.Point(21, 561);
+            this.advancedPIC.Name = "advancedPIC";
+            this.advancedPIC.Size = new System.Drawing.Size(34, 33);
+            this.advancedPIC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.advancedPIC.TabIndex = 11;
+            this.advancedPIC.TabStop = false;
+            // 
+            // empPIC
+            // 
+            this.empPIC.Image = global::Grifindo_Payroll_System.Properties.Resources.corporate;
+            this.empPIC.Location = new System.Drawing.Point(20, 243);
+            this.empPIC.Name = "empPIC";
+            this.empPIC.Size = new System.Drawing.Size(34, 33);
+            this.empPIC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.empPIC.TabIndex = 10;
+            this.empPIC.TabStop = false;
+            // 
+            // salaryPIC
+            // 
+            this.salaryPIC.Image = global::Grifindo_Payroll_System.Properties.Resources.save_money;
+            this.salaryPIC.Location = new System.Drawing.Point(22, 481);
+            this.salaryPIC.Name = "salaryPIC";
+            this.salaryPIC.Size = new System.Drawing.Size(34, 33);
+            this.salaryPIC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.salaryPIC.TabIndex = 9;
+            this.salaryPIC.TabStop = false;
+            // 
+            // bonousPIC
+            // 
+            this.bonousPIC.Image = global::Grifindo_Payroll_System.Properties.Resources.salary;
+            this.bonousPIC.Location = new System.Drawing.Point(22, 403);
+            this.bonousPIC.Name = "bonousPIC";
+            this.bonousPIC.Size = new System.Drawing.Size(34, 33);
+            this.bonousPIC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bonousPIC.TabIndex = 8;
+            this.bonousPIC.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Grifindo_Payroll_System.Properties.Resources.attendance;
+            this.pictureBox5.Location = new System.Drawing.Point(21, 321);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(34, 33);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins SemiBold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 809);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Admin";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Grifindo_Payroll_System.Properties.Resources.people;
+            this.pictureBox3.Location = new System.Drawing.Point(21, 775);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(34, 33);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // homePIC
+            // 
+            this.homePIC.Image = global::Grifindo_Payroll_System.Properties.Resources.home;
+            this.homePIC.Location = new System.Drawing.Point(22, 21);
+            this.homePIC.Name = "homePIC";
+            this.homePIC.Size = new System.Drawing.Size(34, 33);
+            this.homePIC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.homePIC.TabIndex = 2;
+            this.homePIC.TabStop = false;
+            // 
+            // Attendance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1606, 840);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Attendance";
+            this.Palette = this.CommonPal;
+            this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Attendance";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.advancedPIC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.empPIC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.salaryPIC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bonousPIC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.homePIC)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPalette CommonPal;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox advancedPIC;
+        private System.Windows.Forms.PictureBox empPIC;
+        private System.Windows.Forms.PictureBox salaryPIC;
+        private System.Windows.Forms.PictureBox bonousPIC;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox homePIC;
+    }
+}
